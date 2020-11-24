8b89a2dc5e891c7be50f184010a193041b15b80b
Juan Dorado <fdorado985@gmail.com>
2020-11-24 00:22:29 -0600

    Use an @Environment property wrapper to handle the current presentation and dismiss programmatically

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -15,16 +15,18 @@ struct ContentView: View {
       self.showingSheet.toggle()
     }
     .sheet(isPresented: $showingSheet) {
-      SecondView(name: "@fdorado985")
+      SecondView()
     }
   }
 }
 
 struct SecondView: View {
-  var name: String
+  @Environment(\.presentationMode) var presentationMode
 
   var body: some View {
-    Text("Hello, \(name)!")
+    Button("Dismiss") {
+      self.presentationMode.wrappedValue.dismiss()
+    }
   }
 }
 
