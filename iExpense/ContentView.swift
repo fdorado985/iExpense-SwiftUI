7a92bea0f2c2a9bc7fd09cf0551340fd23c75ba1
Juan Dorado <fdorado985@gmail.com>
2020-11-25 01:10:41 -0600

    Create the Expense Item model to save our items

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,22 +7,15 @@
 
 import SwiftUI
 
-struct User: Codable {
-  var firstName: String
-  var lastName: String
+struct ExpenseItem {
+  let name: String
+  let type: String
+  let amount: Int
 }
 
 struct ContentView: View {
-  @State private var user = User(firstName: "Taylor", lastName: "Swift")
-
   var body: some View {
-    Button("Save User") {
-      let encoder = JSONEncoder()
-
-      if let data = try? encoder.encode(self.user) {
-        UserDefaults.standard.set(data, forKey: "User-Data")
-      }
-    }
+    Text("Hello World!")
   }
 }
 
